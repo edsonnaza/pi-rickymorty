5c5850b4133c42fc8ddeaef24181384589798964
edsonnaza <edsonnaza@gmail.com>
2023-10-09 14:35:35 -0300

    Detail and DetailItem

--- src/views/Detail.jsx
+++ src/views/Detail.jsx
@@ -1,5 +1,6 @@
 import { useParams, Link } from "react-router-dom";
 import classes from './Detail.module.css';
+import DetailItem from './DetailItem';
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -30,39 +31,24 @@ const Detail = () =>{
          },[])
      console.log(characters);
     return (
-
-              characters.map((item)=>
-              
-              <div key={item.id} className={classes.wrapper}>
-
-              <div className={classes['product-img']}>
-                <img className={classes.img}   src={item.image}  alt={item.name} />
-              </div>
-
-              <div className={classes['product-info']} >
-                <div className={classes["product-text"]} >
-                    <h1 className={classes['h1']}>{item.name}</h1>
-                    <h2 className={classes['h2']}>{item.species}</h2>
-                    <h2 className={classes.h2}>Status:{item.status}</h2>
-                    <h2>Origin: {item.origin.name} </h2> 
-                    <p>Type: {item.type}</p>
-                </div>
-                <div className={classes["product-price-btn"]}>
-                    <Link to='/'>
-                        <button className={classes.button} type="button">Home</button>
-                    </Link>
-                    
-
-                </div>
-              </div>
-              </div>
-
-              
              
+              characters.map((item)=>( 
+              item.name &&
+              <DetailItem 
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                species={item.species}
+                status={item.status}
+                origin={item.origin.name}
+                type={item.type}
               
+              />
+          
               )
-        
-          )
+        )
+    )
 }
 
 export default Detail;
