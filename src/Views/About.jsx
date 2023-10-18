f5a3f1391c395be591f48336caa758c7626807ee
edsonnaza <edsonnaza@gmail.com>
2023-10-18 11:29:52 -0300

    About scss finished

--- src/Views/About.jsx
+++ src/Views/About.jsx
@@ -1,20 +1,23 @@
-import classes from './About.module.css';
+import classes from './About.module.scss';
+import { Link } from 'react-router-dom';
 const About = (props)=>{
 
     return (
- <section className={classes["about-us"]}>
+ <div className={classes["about-us"]}>
+   <img  src={props.image} className={classes["pic"]} alt="" />
+  
     <div className={classes["about"]}>
-    <img  src={props.image} className={classes["pic"]} alt="" />
-      <div className={classes["text"]}>
+       <div className={classes["text"]}>
         <h2 className={classes.h2}>About Us</h2>
         <h5 className={classes.h5}>Front-end Developer & <span>Designer</span></h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita natus ad sed harum itaque ullam enim quas, veniam accusantium, quia animi id eos adipisci iusto molestias asperiores explicabo cum vero atque amet corporis! Soluta illum facere consequuntur magni. Ullam dolorem repudiandae cumque voluptate consequatur consectetur, eos provident necessitatibus reiciendis corrupti!</p>
         <div className={classes["data"]}>
-        <a href="#" className={classes["hire"]}>Hire Me</a>
-        </div>
+          < Link to='/' >
+        <button  className={classes["hire"]}>Home</button>
+        </Link> </div>
       </div>
     </div>
-  </section>
+  </div>
     )
 }
 
